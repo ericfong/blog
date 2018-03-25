417d0f06da7ef290aba65b4912af04331ecffbb6
Eric <ericff@gmail.com>
2018-03-26 01:30:06 +0800

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-# hugo # if using a theme, replace with `
 hugo -t even
 
+
+cd docs
 # Add changes to git.
 git add .
 
